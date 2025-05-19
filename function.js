a5fba418ecbe7a1026ae4480bef838b665295a1e
unknown <BuddhiswarMondal.>
2025-05-19 13:06:30 -0800

    my all previous work

---
+++ function.js
@@ -0,0 +1,21 @@
+var page=document.getElementsByClassName("page")[0];
+page.style.width="100vw";
+page.style.height="100vh";
+page.style.backgroundColor="aqua";
+
+
+
+
+
+
+// from here h1(id="headHeading") is manupulated....
+var mainHeading =document.getElementById("headHeading");
+mainHeading.style.color="green";
+mainHeading.style.textShadow="10px 10px 10px black";
+mainHeading.style.backgroundColor="crimson";
+mainHeading.style.textAlign="center";
+mainHeading.style.fontSize="60px";
+
+
+// End of h1(id="headHeading")  manupulation...
+
